269405a3a34dd9b13d1361d1ea0d5fc60314bda8
Hannah Moss <46806107+mosshan@users.noreply.github.com>
2021-03-15 10:02:15 -0400

    Updated Blockchain Single Threaded Java- Testing IP
    Reflection of current Repl.it version

--- Java Blockchain/BlockchainSingleThreaded.java
+++ Java Blockchain/BlockchainSingleThreaded.java
@@ -1,6 +1,6 @@
 import java.math.BigInteger;
 import java.security.MessageDigest;
-import java.util.Scanner;				// Import the Scanner class
+import java.util.Scanner; // Import the Scanner class
 import java.util.Date;
 import java.util.*;
 
@@ -12,30 +12,53 @@ public class BlockchainSingleThreaded {
 	static int prefix = 4;
 	
 	// main method
-	public static void main(String[] args){
-		
+	public static void main(String[] args) {
 		// Print to screen creation of genesis block
 		System.out.println("creating genesis block...");
-		// Create the data for the genesis block
-		String genesisData = new String("The Times 03/Jan/2009 Chancellor on brink of second bailout for banks");
-		
+
+		Scanner in = new Scanner(System.in);
+		String data = null;
+		// Grab the first file line as data for the genesis block
+		data = in.nextLine();
+
 		// Instantiate the genesis block
-		Block genesisBlock = new Block(genesisData, null, new Date().getTime());
-		
+		Block genesisBlock = new Block(data, "", new Date().getTime());
+		// Mine the genesis block, to create the hash for our next block
+		genesisBlock.mineBlock(prefix);
+		// Validate our newly mined genesis block, using null as prev hash
+		while(!genesisBlock.validateBlock(blockchain, prefix))
+		{
+			// If our validation fails retry mining after resetting values
+			genesisBlock.setNonce(0);
+			genesisBlock.setTimeStamp(new Date().getTime());
+		genesisBlock.mineBlock(prefix);
+		}
 		// Add genesis block to the blockchain
 		blockchain.add(genesisBlock);
-		
-		/* Continue until we reach the end of the input file
-		while(){
-			Loop through input file, reading in line by line of text
-            Use block constructor and send it new line of data
-            mineBlock on newly created block
-            upon return, validate created block
-                - Should we replicate what would logically occur should this verification fail?
-                    (even though we know ours won't)
-		}*/
-		
+		String previousHash = genesisBlock.getHash();
+
+		// Continue until we reach the end of the input file
+		while(in.hasNextLine()){
+			// Read in line
+			data = in.nextLine();
+			// Create a new block with our line of data
+			Block newBlock = new Block(data, previousHash, new Date().getTime());
+			newBlock.mineBlock(prefix);
+			while(!newBlock.validateBlock(blockchain, prefix))
+			{
+				// If our validation fails retry mining after resetting values
+				newBlock.setNonce(0);
+				newBlock.setTimeStamp(new Date().getTime());
+				newBlock.mineBlock(prefix);
+			}
+			// Add our newly mined and verified block and set up values for next block
+			blockchain.add(newBlock);
+			previousHash = newBlock.getHash();
+	
+			} 
+
 		System.out.println(blockchain.get(0).getData());
+		in.close();
 	}
 	
 }
@@ -56,6 +79,7 @@ class Block{
 		this.previousHash = previousHash;
 		this.timeStamp = timeStamp;
 		this.hash = calculateBlockHash();
+    this.nonce = 0;
 	}
 	
 	// Standard getters
@@ -127,7 +151,7 @@ class Block{
 	}
 	
 	// Method to mine a block
-	public String mineBlock(int prefix){
+	public void mineBlock(int prefix){
 		// Define the prefix we want to find
 		String prefixString = new String(new char[prefix]).replace('\0', '0');
 		
@@ -137,21 +161,37 @@ class Block{
 			nonce++;
 			// Calculate another block hash
 			hash = calculateBlockHash();
-        }	
-		return hash;
+    }		
+    return;
 	}
-	
-	/* Method to build a block
-	public void buildBlock() {
-		
-	}
-	*/
-	
-	
-	/* Method to validat the block
-	public boolean validateBlock() {
-		
-	}
-	*/
 
+  // Method to validate the block
+	public boolean validateBlock(List<Block> blockchain, int prefix) {
+    // Initialize a boolean value to true
+    boolean valid = true;  
+
+    // The prefix string we want to match our hash prefix to   
+    String prefixString = new String(new char[prefix]).replace('\0', '0');
+
+    // Local variable to store the size of the blockchain
+    int size = blockchain.size();
+
+    // If this blockchain is NOT empty then we are NOT validating the genesis block
+    if(size >= 1){
+      // Validate the previousHash, hash and prefix
+      valid = (previousHash.equals(blockchain.get(size - 1).previousHash))
+            && (hash.equals(calculateBlockHash()))
+            && (hash.startsWith(prefixString));
+    }
+
+    // Otherwise it MUST be the genesis block since the blockchain is empty
+    else {
+       valid = (previousHash.equals(""))
+            && (hash.equals(calculateBlockHash()))
+            && (hash.startsWith(prefixString));
+    }    
+      
+    // Return the value of valid
+    return valid;
+	}
 }
