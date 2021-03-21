cd23863f2287fba2cb9dd3af33ef38f016c4c848
Hannah Moss <46806107+mosshan@users.noreply.github.com>
2021-03-21 19:35:16 -0400

    Resolved Validation + Hash string building issues -> Testing successful

--- Java Blockchain/BlockchainSingleThreaded.java
+++ Java Blockchain/BlockchainSingleThreaded.java
@@ -1,4 +1,3 @@
-import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.util.Scanner; // Import the Scanner class
 import java.util.Date;
@@ -9,8 +8,8 @@ import java.util.*;
 public class BlockchainSingleThreaded {
 	static List<Block> blockchain = new ArrayList<>();
     // Prefix dictates the difficulty of block mining, the higher the prefix, the more 0s required at the beginning of a block header hash
-	static int prefix = 4;
-	
+	static int prefix = 1;
+
 	// main method
 	public static void main(String[] args) {
 		// Print to screen creation of genesis block
@@ -31,7 +30,7 @@ public class BlockchainSingleThreaded {
 			// If our validation fails retry mining after resetting values
 			genesisBlock.setNonce(0);
 			genesisBlock.setTimeStamp(new Date().getTime());
-		genesisBlock.mineBlock(prefix);
+			genesisBlock.mineBlock(prefix);
 		}
 		// Add genesis block to the blockchain
 		blockchain.add(genesisBlock);
@@ -53,26 +52,28 @@ public class BlockchainSingleThreaded {
 			}
 			// Add our newly mined and verified block and set up values for next block
 			blockchain.add(newBlock);
+			//System.out.println(newBlock.getData());
 			previousHash = newBlock.getHash();
-	
-			} 
 
-		System.out.println(blockchain.get(0).getData());
+			}
+
+		System.out.println("Blockchain Complete");
+		System.out.println("Number of blocks added: " + blockchain.size());
 		in.close();
 	}
-	
+
 }
 
 // Block class
 class Block{
-	
+
 	// Block properties
 	private String hash;
 	private String previousHash;
 	private String data;
 	private long timeStamp;
 	private int nonce;
-	
+
 	// Block constructor
 	public Block(String data, String previousHash, long timeStamp){
 		this.data = data;
@@ -81,45 +82,45 @@ class Block{
 		this.hash = calculateBlockHash();
     this.nonce = 0;
 	}
-	
+
 	// Standard getters
 	public String getHash(){
 		return this.hash;
 	}
-	
+
 	public String getPreviousHash(){
 		return this.previousHash;
 	}
-	
+
 	public String getData(){
 		return this.data;
 	}
-	
+
 	public long timeStamp(){
 		return this.timeStamp;
 	}
-	
+
 	public int getNonce(){
 		return this.nonce;
 	}
-	
+
 	// Standard setters
 	public void setHash(){
 		this.hash = calculateBlockHash();
 	}
-	
+
 	public void setPreviousHash(String previousHash){
 		this.previousHash = previousHash;
 	}
-	
+
 	public void setData(String data){
 		this.data = data;
 	}
-	
+
 	public void setTimeStamp(long timeStamp){
 		this.timeStamp = timeStamp;
 	}
-	
+
 	public void setNonce(int nonce){
 		this.nonce = nonce;
 	}
@@ -132,45 +133,47 @@ class Block{
 			+ Long.toString(timeStamp)
 			+ Integer.toString(nonce)
 			+ data;
-		
+
         // Initialize the byte array
         byte[] byteRep = null;
-        String hashedBlock = null;
 
         try {
             // Create a new MessageDigest object which implements SHA-256
             MessageDigest md = MessageDigest.getInstance("SHA-256");
             // Generate the hash value of our input data, which is a byte array
             byteRep = md.digest(dataToHash.getBytes());
-            // Convert byte array to correctly formatted string
-            hashedBlock = String.format("%x", new BigInteger(1, byteRep));
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return hashedBlock;
+		// Convert byte array to correctly formatted string
+        StringBuffer buffer = new StringBuffer();
+    	for (byte b : byteRep) {
+        	buffer.append(String.format("%02x", b));
+    	}
+    	return buffer.toString();
 	}
-	
+
 	// Method to mine a block
 	public void mineBlock(int prefix){
 		// Define the prefix we want to find
 		String prefixString = new String(new char[prefix]).replace('\0', '0');
-		
+
 		// Until we find a hash beginning with the correct prefix, meaning we have found a hash smaller than our necessary target
 		while(!hash.substring(0, prefix).equals(prefixString)){
 			// Increment the nonce
 			nonce++;
 			// Calculate another block hash
 			hash = calculateBlockHash();
-    }		
+    }
     return;
 	}
 
   // Method to validate the block
 	public boolean validateBlock(List<Block> blockchain, int prefix) {
     // Initialize a boolean value to true
-    boolean valid = true;  
+    boolean valid = true;
 
-    // The prefix string we want to match our hash prefix to   
+    // The prefix string we want to match our hash prefix to
     String prefixString = new String(new char[prefix]).replace('\0', '0');
 
     // Local variable to store the size of the blockchain
@@ -179,9 +182,12 @@ class Block{
     // If this blockchain is NOT empty then we are NOT validating the genesis block
     if(size >= 1){
       // Validate the previousHash, hash and prefix
-      valid = (previousHash.equals(blockchain.get(size - 1).previousHash))
+      valid = (previousHash.equals(blockchain.get(size - 1).hash))
             && (hash.equals(calculateBlockHash()))
             && (hash.startsWith(prefixString));
+		//System.out.println("prev hash  " + previousHash + " my prv hash" + blockchain.get(size - 1).hash);
+		//System.out.println("my hash  " + hash + " recalced hash" + calculateBlockHash());
+		//System.out.println("starting w prefix  " + hash.startsWith(prefixString));
     }
 
     // Otherwise it MUST be the genesis block since the blockchain is empty
@@ -189,8 +195,9 @@ class Block{
        valid = (previousHash.equals(""))
             && (hash.equals(calculateBlockHash()))
             && (hash.startsWith(prefixString));
-    }    
-      
+
+    }
+
     // Return the value of valid
     return valid;
 	}
