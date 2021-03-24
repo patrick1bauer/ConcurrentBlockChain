514ede759728c5e2d19b6cad66a206b00b079757
StaceyD22 <stacey.dale2311@gmail.com>
2021-03-24 16:59:24 -0400

    update haskell blockchain

--- Haskell Blockchain/src/SingleThreadedBlockChain.hs
+++ Haskell Blockchain/src/SingleThreadedBlockChain.hs
@@ -12,14 +12,11 @@ data Block = Block { contents :: String
              } deriving (Show)
 
 genesisBlock :: String -> Block
-genesisBlock time = Block contents hash previousHash time Nothing
+genesisBlock time = mineBlock (Block contents hash previousHash time Nothing) 0
                  where 
-                     contents = "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks"
+                     contents = "In the beginning, there was Plato's Gorgias"
                      hash = ""
                      previousHash = "000000000000000000000000000000000"
-
-newBlockChain :: String -> Block
-newBlockChain time = mineBlock (genesisBlock time) 0
 
 
 hashBlock :: Block -> String
@@ -36,7 +33,7 @@ mineBlock b@(Block c _ p t _) n = case head pow of
           pow = toString . Base16.encode $ SHA256.finalize ctx -- proof of work
 
 getHash :: Block -> String
-getHash (Block _ _ p _ _) = p
+getHash (Block _ h _ _ _) = h
 
 addBlock :: String -> String -> String -> Block
 addBlock p newContent time = mineBlock (Block newContent "" p time Nothing) 0
@@ -45,7 +42,7 @@ makeBlockChain :: String -> [String] -> Int -> String -> [Block]
 makeBlockChain p [] _ _= [];
 makeBlockChain p story 0 time = [firstBlock] ++ makeBlockChain pHash story 1 time
                              where
-                                 firstBlock = newBlockChain time
+                                 firstBlock = genesisBlock time
                                  pHash = getHash firstBlock
 makeBlockChain p (s:story) n time = [newBlock] ++ (makeBlockChain pHash story (n+1) time)
                                  where  
