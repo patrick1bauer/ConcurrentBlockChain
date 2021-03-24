2be54bc28b2379810fb37b93fc7adb5bfaf4b4c1
StaceyD22 <stacey.dale2311@gmail.com>
2021-03-24 19:17:14 -0400

    update Haskell Blockchain

--- Haskell Blockchain/src/Test.hs
+++ Haskell Blockchain/src/Test.hs
@@ -1,4 +1,4 @@
-module SingleThreadedBlockChain where
+module Test where
 
 import Data.ByteString.UTF8 (fromString, toString)
 import qualified Crypto.Hash.SHA256     as SHA256
@@ -18,35 +18,15 @@ genesisBlock time = mineBlock (Block contents hash previousHash time Nothing) 0
                      hash = ""
                      previousHash = "000000000000000000000000000000000"
 
-
 hashBlock :: Block -> String
 hashBlock (Block contents hash prevHash timeStamp _) = toString $ Base16.encode digest
     where ctx = SHA256.updates SHA256.init $ fmap fromString [contents, prevHash, timeStamp]
           digest = SHA256.finalize ctx 
 
 mineBlock :: Block -> Int -> Block
-mineBlock b@(Block c _ p t _) n = if (foldl1 (&&) [x=='0' | x <- (take 4 pow)])
+mineBlock b@(Block c _ p t _) n = if (foldl1 (&&) [x=='0' | x <- (take 2 pow)])
                                      then Block c blockHash p t (Just n)
                                   else mineBlock b (n + 1)
     where blockHash = hashBlock b
           ctx = SHA256.updates SHA256.init (fmap fromString [blockHash, show n, p])
           pow = toString . Base16.encode $ SHA256.finalize ctx 
-
-getHash :: Block -> String
-getHash (Block _ h _ _ _) = h
-
-addBlock :: String -> String -> String -> Block
-addBlock p newContent time = mineBlock (Block newContent "" p time Nothing) 0
-
-makeBlockChain :: String -> [String] -> Int -> String -> [Block]
-makeBlockChain p [] _ _= [];
-makeBlockChain p story 0 time = [firstBlock] ++ makeBlockChain pHash story 1 time
-                             where
-                                 firstBlock = genesisBlock time
-                                 pHash = getHash firstBlock
-makeBlockChain p (s:story) n time = [newBlock] ++ (makeBlockChain pHash story (n+1) time)
-                                 where  
-                                    newBlock = addBlock p s time
-                                    pHash = getHash newBlock
-                    
-
