0c273d4aff7d7bc4811063019cf24f77ae69f70e
Hannah Moss <46806107+mosshan@users.noreply.github.com>
2021-04-18 16:43:03 -0400

    Update BlockchainMultiThreaded.java
    - Fixed validation repeats

--- Java Blockchain/Multi/BlockchainMultiThreaded.java
+++ Java Blockchain/Multi/BlockchainMultiThreaded.java
@@ -18,6 +18,7 @@ public class BlockchainMultiThreaded{
 	public boolean checked = false;
 	public volatile boolean timeToCheck = false;
 	public volatile boolean timeToCalc = false;
+	public volatile int threadWhoFound = -1;
 
 
 	// main method
@@ -76,10 +77,14 @@ public class BlockchainMultiThreaded{
 		while(!chain.timeToCheck)
 		{
 		}
+
+		genesisBlock.setNonce(computers.get(chain.threadWhoFound - 1).nonce);
+		genesisBlock.setHash();
 		
 		// Validate our newly mined genesis block, using null as prev hash
 		while(!genesisBlock.validateBlock(chain.blockchain, prefix))
 		{
+			System.out.println("lol rip 1");
 			// Let the threads know the nonce has been checked and has not been found
 			chain.timeToCheck = false;
 			chain.nonceFound = false;
@@ -103,6 +108,9 @@ public class BlockchainMultiThreaded{
 			{
 
 			}
+
+			genesisBlock.setNonce(computers.get(chain.threadWhoFound - 1).nonce);
+			genesisBlock.setHash();
 
 		}
 		
@@ -145,9 +153,13 @@ public class BlockchainMultiThreaded{
 
 			}
 
+			newBlock.setNonce(computers.get(chain.threadWhoFound - 1).nonce);
+			newBlock.setHash();
 			
 			while(!newBlock.validateBlock(chain.blockchain, prefix))
 			{
+
+				System.out.println("lol rip" + newBlock.getNonce() + "  "+ newBlock.getHash() + " " + chain.threadWhoFound);
 				// Let the threads know the nonce has been checked and has not been found
 				chain.timeToCheck = false;
 				chain.nonceFound = false;
@@ -169,8 +181,10 @@ public class BlockchainMultiThreaded{
 				// Wait until we have possibly found the hash
 				while(!chain.timeToCheck)
 				{
-
 				}
+
+				newBlock.setNonce(computers.get(chain.threadWhoFound - 1).nonce);
+				newBlock.setHash();
 			}
 
 			// Add our newly mined and verified block and set up values for next block
@@ -217,7 +231,8 @@ public class BlockchainMultiThreaded{
         return temp;
     }
 
-	public synchronized void setBlockAndStop() {
+	public synchronized void setBlockAndStop(int ID) {
+		threadWhoFound = ID;
         timeToCheck = true;
 		timeToCalc = false;
 		nonceFound = true;
@@ -348,5 +363,106 @@ class Block{
 	}
 
 }
+
+class Computer implements Runnable{
+    
+	public boolean chainComplete = false;
+
+	public Block currBlock;
+    public int ID;
+	private BlockchainMultiThreaded chain;
+    public int nonce;
+    
+
+    public int prefix;
+
+    public Computer(int ID, BlockchainMultiThreaded chain, int prefix) {
+		this.ID = ID;
+		this.chain = chain;
+        this.prefix = prefix;
+	}
+
+    public void stop() {
+		chainComplete = true;
+	}
+
+    public void run()
+	{
+		while(!chainComplete)
+		{
+			// Wait until it is time to calculate a hash
+			if(chain.timeToCalc)
+			{
+                mineBlock(prefix);
+                if(!chain.nonceFound)
+                {
+					chain.setBlockAndStop(ID);
+                }
+                else
+                {
+                    return;
+                }
+			}
+            else
+            {
+
+            }
+		}
+	
+    }
+
+	
+
+    // Method to mine a block
+	public void mineBlock(int prefix){
+		// Define the prefix we want to find
+		String prefixString = new String(new char[prefix]).replace('\0', '0');
+        nonce = chain.getNonce();
+		// Until we find a hash beginning with the correct prefix, meaning we have found a hash smaller than our necessary target
+		while(!calculateBlockHash().substring(0, prefix).equals(prefixString)){
+			if(chain.nonceFound)
+			{
+				return;
+			}
+			// Grab the next nonce
+            nonce = chain.getNonce();
+			// Calculate another block hash
+    	}
+
+	}
+
+    // Method to calculate block hash
+	public String calculateBlockHash(){
+		// Concatenate parts of the block to generate the hash
+		String dataToHash = currBlock.getPreviousHash()
+			+ Long.toString(currBlock.timeStamp())
+			+ nonce
+			+ currBlock.getData();
+
+        // Initialize the byte array
+        byte[] byteRep = null;
+
+        try {
+            // Create a new MessageDigest object which implements SHA-256
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            // Generate the hash value of our input data, which is a byte array
+            byteRep = md.digest(dataToHash.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+		// Convert byte array to correctly formatted string
+        StringBuffer buffer = new StringBuffer();
+    	for (byte b : byteRep) {
+        	buffer.append(String.format("%02x", b));
+    	}
+    	return buffer.toString();
+	}
+
+	public void setBlock(Block currentBlock)
+	{
+		this.currBlock = currentBlock;
+	}
+}
+
 
 
